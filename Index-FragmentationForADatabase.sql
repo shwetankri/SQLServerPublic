af13e61a4d8089da64bfe09f05732d41dca00611
shwetankri <47349982+shwetankri@users.noreply.github.com>
2020-03-03 00:27:21 +0530

    updated query formatting

--- Index-FragmentationForADatabase.sql
+++ Index-FragmentationForADatabase.sql
@@ -1,10 +1,18 @@
 --Query to check the fragmentation level for all the indexes having 1000+ pages
 
-SELECT DB_NAME(ps.database_id) AS [Database Name], SCHEMA_NAME(o.[schema_id]) AS [Schema Name],
-OBJECT_NAME(ps.OBJECT_ID) AS [Object Name], i.[name] AS [Index Name], ps.index_id, 
-ps.index_type_desc, ps.avg_fragmentation_in_percent, 
-ps.fragment_count, ps.page_count, i.fill_factor, i.has_filter, 
-i.filter_definition, i.[allow_page_locks]
+SELECT DB_NAME(ps.database_id) AS [Database Name], 
+SCHEMA_NAME(o.[schema_id]) AS [Schema Name],
+OBJECT_NAME(ps.OBJECT_ID) AS [Object Name], 
+i.[name] AS [Index Name], 
+ps.index_id, 
+ps.index_type_desc, 
+ps.avg_fragmentation_in_percent, 
+ps.fragment_count, 
+ps.page_count, 
+i.fill_factor, 
+i.has_filter, 
+i.filter_definition, 
+i.[allow_page_locks]
 FROM sys.dm_db_index_physical_stats(DB_ID(),NULL, NULL, NULL , N'LIMITED') AS ps
 INNER JOIN sys.indexes AS i WITH (NOLOCK)
 ON ps.[object_id] = i.[object_id] 
