9bc9af62b338d94f80986fc90dcacdc7e94e85ec
shwetankri <47349982+shwetankri@users.noreply.github.com>
2020-03-02 23:58:02 +0530

    Create Statistics-Checkcurrentstatsdate.sql

---
+++ Statistics-Checkcurrentstatsdate.sql
@@ -0,0 +1,19 @@
+--Script to check the stats date for the current database
+
+
+SELECT SCHEMA_NAME(o.Schema_ID) + N'.' + o.[NAME] AS [Object Name], o.[type_desc] AS [Object Type],
+      i.[name] AS [Index Name], STATS_DATE(i.[object_id], i.index_id) AS [Statistics Date], 
+      s.auto_created, s.no_recompute, s.user_created, s.is_incremental, s.is_temporary,
+	  st.row_count, st.used_page_count
+FROM sys.objects AS o WITH (NOLOCK)
+INNER JOIN sys.indexes AS i WITH (NOLOCK)
+ON o.[object_id] = i.[object_id]
+INNER JOIN sys.stats AS s WITH (NOLOCK)
+ON i.[object_id] = s.[object_id] 
+AND i.index_id = s.stats_id
+INNER JOIN sys.dm_db_partition_stats AS st WITH (NOLOCK)
+ON o.[object_id] = st.[object_id]
+AND i.[index_id] = st.[index_id]
+WHERE o.[type] IN ('U', 'V')
+AND st.row_count > 0
+ORDER BY STATS_DATE(i.[object_id], i.index_id) DESC OPTION (RECOMPILE);
