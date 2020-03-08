b6828f206215ecaedb0ca9745380876af96e0e4d
shwetankri <47349982+shwetankri@users.noreply.github.com>
2020-03-08 23:52:47 +0530

    Create TSQL-GenreateScripts.sql

---
+++ TSQL-GenreateScripts.sql
@@ -0,0 +1,9 @@
+#Use below to Grant execute permission to all the stored procedure to user test
+
+#This is basically know-how to use SELECT with concatenating to generate scripts
+
+SELECT ob.name, * , 'GRANT EXECUTE ON ' + sc.name + '.' + ob.name + ' TO test' 
+FROM sys.objects ob
+JOIN sys.schemas sc
+ON ob.schema_id = sc.schema_id
+WHERE type_desc = 'SQL_STORED_PROCEDURE'
