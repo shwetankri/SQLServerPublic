666f3153b490ca9188b05fec7fcbe1f6a2e92baa
shwetankri <47349982+shwetankri@users.noreply.github.com>
2020-05-24 09:47:47 +0530

    Update TSQL-GenreateScripts.sql

--- TSQL-GenreateScripts.sql
+++ TSQL-GenreateScripts.sql
@@ -1,6 +1,6 @@
 #Use below to Grant execute permission to all the stored procedure to user test
 
-#This is basically know-how to use SELECT with concatenating to generate scripts
+#This is basically know-how to use SELECT with concatenation to generate scripts
 
 SELECT ob.name, * , 'GRANT EXECUTE ON ' + sc.name + '.' + ob.name + ' TO test' 
 FROM sys.objects ob
